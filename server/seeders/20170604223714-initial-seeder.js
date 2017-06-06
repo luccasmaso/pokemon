26b0f5b887ae94a39c487786aa1700a95d53b115
Luccas Maso <luccas.no@gmail.com>
2017-06-06 02:47:51 -0300

    Seed timestamp fix when bulk insert

--- server/seeders/20170604223714-initial-seeder.js
+++ server/seeders/20170604223714-initial-seeder.js
@@ -2,16 +2,17 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
+    var date = new Date()
     return queryInterface.bulkInsert('pokemons', [
-      {name: 'Pikachu', price: 10.55},
-      {name: 'Bullbasaur', price: 21.99},
-      {name: 'Charmander', price: 30.08},
-      {name: 'Snorlax', price: 40.12},
-      {name: 'Squirtle', price: 28.00},
-      {name: 'Zubat', price: 28.00},
-      {name: 'Jigglypuff', price: 28.00},
-      {name: 'Bellsprout', price: 28.00},
-      {name: 'Psyduck', price: 28.00}
+      {name: 'Pikachu', price: 10.55, created_at: date, updated_at: date},
+      {name: 'Bullbasaur', price: 21.99, created_at: date, updated_at: date},
+      {name: 'Charmander', price: 30.08, created_at: date, updated_at: date},
+      {name: 'Snorlax', price: 40.12, created_at: date, updated_at: date},
+      {name: 'Squirtle', price: 28.00, created_at: date, updated_at: date},
+      {name: 'Zubat', price: 28.00, created_at: date, updated_at: date},
+      {name: 'Jigglypuff', price: 28.00, created_at: date, updated_at: date},
+      {name: 'Bellsprout', price: 28.00, created_at: date, updated_at: date},
+      {name: 'Psyduck', price: 28.00, created_at: date, updated_at: date}
     ], {})
   },
 
